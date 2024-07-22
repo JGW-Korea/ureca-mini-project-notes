1e432999ad805b716577fd8e0776a35eac99d4a8
JGW-Korea <jgw6372@gmail.com>
2024-07-22 20:30:48 +0900

    vscode 스프링 부트 프로젝트  초기화

---
+++ src/main/java/com/notes/ureca_mini_project_notes/UrecaMiniProjectNotesApplication.java
@@ -0,0 +1,13 @@
+package com.notes.ureca_mini_project_notes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrecaMiniProjectNotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrecaMiniProjectNotesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/notes/ureca_mini_project_notes/UrecaMiniProjectNotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.notes.ureca_mini_project_notes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrecaMiniProjectNotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
